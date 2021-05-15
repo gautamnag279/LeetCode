dac6041459c637a46e867441a06025af675267b8
Gautam Nag <51375618+gautamnag279@users.noreply.github.com>
2021-05-15 21:24:58 +0530

    Update Add Binary.py

--- Easy/Add Binary.py
+++ Easy/Add Binary.py
@@ -1,12 +1,12 @@
-#THIS WAS ONLY FASTER THAN 5% OF THE SOLUTIONS, LOL! 
+#THIS WAS ONLY FASTER THAN 5% OF THE SOLUTIONS
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         add = int(a , 2) + int(b , 2)
         return bin(add)[2:]
 
     
-#WHEREAS IF I DO THIS, IT MAKES IT FASTER THAN 97% OF THE SOLUTION...LIKE WTF!
+#BUT IF I DO THIS, IT MAKES IT FASTER THAN 97% OF THE SOLUTIONS...LIKE WTF!
 class Solution:
-def addBinary(self, a: str, b: str) -> str:
-    tot = int(a, 2) + int(b, 2)
-    return bin(tot).replace("0b", "")
+    def addBinary(self, a: str, b: str) -> str:
+        add = int(a, 2) + int(b, 2)
+        return bin(add).replace("0b", "")
