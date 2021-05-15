d52a6276059cf1b59d1aed6dc5cab22f892ee32e
Gautam Nag <51375618+gautamnag279@users.noreply.github.com>
2021-05-15 21:25:09 +0530

    Update Add Binary.py

--- Easy/Add Binary.py
+++ Easy/Add Binary.py
@@ -4,7 +4,6 @@ class Solution:
         add = int(a , 2) + int(b , 2)
         return bin(add)[2:]
 
-    
 #BUT IF I DO THIS, IT MAKES IT FASTER THAN 97% OF THE SOLUTIONS...LIKE WTF!
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
