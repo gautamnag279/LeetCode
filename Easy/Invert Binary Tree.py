7e7da4410bd004fb9bb6d34226c14e50ae8d2680
Gautam Nag <51375618+gautamnag279@users.noreply.github.com>
2021-07-02 17:02:01 +0530

    Update Invert Binary Tree.py

--- Easy/Invert Binary Tree.py
+++ Easy/Invert Binary Tree.py
@@ -1,11 +1,32 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
         if root:
             root.left , root.right = self.invertTree(root.right) , self.invertTree(root.left)
             return root
+        
+        
+#This is he same ode but it is easy to understand  
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        
+        if root:
+            # General case:
+            
+            # invert child node of current root
+            root.left, root.right = root.right, root.left
+            
+            # invert subtree with DFS
+            
+            if root.left:
+                self.invertTree( root.left )
+            
+            if root.right:
+                self.invertTree( root.right )
+            
+            return root
+        
+        else:
+            # Base case:
+            # empty tree
+            
+            return None
