72cf3833498d4509a68d0c495fdab87c64a2ace3
Gautam Nag <51375618+gautamnag279@users.noreply.github.com>
2021-07-09 16:03:02 +0530

    Update Reverse Bits.py

--- Easy/Reverse Bits.py
+++ Easy/Reverse Bits.py
@@ -8,3 +8,4 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         return int('{0:032b}'.format(n)[::-1] , 2)
                    
+https://stackoverflow.com/questions/29683207/missing-zeros-when-using-format-to-make-an-integer-to-a-binary
